5d7639d3768bfcc8ee54bec1266c2151041698f6
ningweiwang <ningweiwang@sohu-inc.com>
2020-05-21 17:53:10 +0800

    add:initConfig

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
-const CompressionPlugin = require("compression-webpack-plugin")
+const CompressionPlugin = require("compression-webpack-plugin");
 module.exports = {
   publicPath: './',
-  outputDir: 'dist',
-  indexPath: 'index.html',
-	filenameHashing: false,
+  filenameHashing: false,
+  crossorigin:undefined, // cors
   chainWebpack: config => {
     config
 			.plugin('html')
@@ -12,16 +11,23 @@ module.exports = {
 				return args
 			})
   },
-  // configureWebpack: {
-  //   plugins:[
-  //     new CompressionPlugin({
-  //       filename: '[path].gz[query]',
-  //       algorithm: 'gzip',
-  //       test: /\.js$|\.html$|\.json$|\.css/,
-  //       threshold: 0, // 只有大小大于该值的资源会被处理
-  //       minRatio:0.8, // 只有压缩率小于这个值的资源才会被处理
-  //       deleteOriginalAssets: false // 删除原文件
-  //     })
-  //   ],
-  // },
+  configureWebpack: config =>{
+    if (process.env.NODE_ENV === 'production') {
+      const plugins = []
+      plugins.push(
+        new CompressionPlugin({
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: /\.css$|\.ttf$|\.html$|\.svg$|\.json$|\.js$/,
+          threshold: 0, // 只有大小大于该值的资源会被处理
+          minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
+          deleteOriginalAssets: true// 删除原文件
+        }),
+      )
+      config.plugins = [
+        ...config.plugins,
+        ...plugins
+      ]
+    }
+  },
 }
