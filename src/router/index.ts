e6dc7fe0aa6a258f52186efd9bcc1cc6e88f5303
ningweiwang <ningweiwang@sohu-inc.com>
2020-05-28 16:11:12 +0800

    fix:router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,27 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes: RouteConfig[] = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    redirect: '/index',
+  },
+  {
+    path: '/index',
+    name: 'Index',
+    component: () => import('../views/Home.vue'),
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import('../views/About.vue'),
+  },
+  {
+    path: '/aiding',
+    name: 'Python',
+    component: () => import('../views/Python.vue'),
   },
 ];
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,23 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button @click="route('about')">About</el-button>
+    <el-button @click="route('aiding')">Aiding</el-button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+  },
+  methods: {
+    route(page) {
+      this.$router.push({path: '/' + page});
+    },
   },
 };
 </script>
