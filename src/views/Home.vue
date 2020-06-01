53164fb3c107ca3b1fd990e6ffa4f6bd84819844
ningweiwang <ningweiwang@sohu-inc.com>
2020-06-01 21:01:44 +0800

    change:home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <el-button @click="route('about')">About</el-button>
+    <img alt="Vue logo" src="http://www.anlan.xyz:8080/init/anlan.jpg">
+    <HelloWorld msg="QQ：2016751527"/>
+    <el-button @click="route('about')">待开发</el-button>
     <el-button @click="route('aiding')">爱锭网（重构中）</el-button>
   </div>
 </template>
@@ -22,3 +22,10 @@ export default {
   },
 };
 </script>
+
+<style lang="less" scoped>
+img {
+  width: 300px;
+  margin-top: 50px;
+}
+</style>
