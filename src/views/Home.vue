6afa8f772f3f976ee61951824e7dd6d04dcc3057
ningweiwang <ningweiwang@sohu-inc.com>
2020-06-02 17:05:22 +0800

    add:ad-aiding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="http://www.anlan.xyz:8080/init/anlan.jpg">
+    <img class="logo" src="http://www.anlan.xyz:8080/init/anlan.jpg">
     <HelloWorld msg="QQ：2016751527"/>
     <el-button @click="route('about')">全民大乐斗（待开发）</el-button>
     <el-button @click="route('aiding')">爱锭网（重构中）</el-button>
-    <el-button @click="aiding">爱锭网（跳转）</el-button>
+    <el-tooltip placement="top">
+      <div slot="content">
+        <p>扫右侧二维码加好友</p>
+        <p>填写观棋不宇推荐即可</p>
+      </div>
+      <el-button @click="aiding">爱锭网（跳转）</el-button>
+    </el-tooltip>
+    <section class="ad">
+      <img src="http://www.anlan.xyz:8080/images/aiding.jpg">
+    </section>
   </div>
 </template>
 
@@ -28,8 +37,24 @@ export default {
 </script>
 
 <style lang="less" scoped>
-img {
+.logo {
   width: 300px;
   margin-top: 50px;
+}
+.ad {
+  width: 233px;
+  height: 660px;
+  overflow-y: auto;
+  position: fixed;
+  top: 50%;
+  right: 30px;
+  margin-top: -330px;
+  &::-webkit-scrollbar {
+    background: transparent;
+  }
+  img {
+    width: 216px;
+    height: 1320px;
+  }
 }
 </style>
