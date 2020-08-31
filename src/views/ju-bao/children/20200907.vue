343c26eab6b233d415fec0aa3248ef32d278d367
Poison-Frog <46836317+Poison-Frog@users.noreply.github.com>
2020-09-01 00:17:21 +0800

    update:jubao

--- src/views/ju-bao/children/20200907.vue
+++ src/views/ju-bao/children/20200907.vue
@@ -17,6 +17,18 @@ export default {
       day: '7',
       tableData: [
         {
+          area: '微信小程序1区',
+          uid: '1593411',
+          img: 'http://anlan.xyz:8080/images/jubao/20200907/004.jpg',
+          times: '初犯',
+        },
+        {
+          area: '微信小程序1区',
+          uid: '1209134',
+          img: 'http://anlan.xyz:8080/images/jubao/20200907/005.jpg',
+          times: '第2次',
+        },
+        {
           area: '微信小程序10区',
           uid: '30150159',
           img: 'http://anlan.xyz:8080/images/jubao/20200907/001.jpg',
