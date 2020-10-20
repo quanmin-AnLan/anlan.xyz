51d08baae74f5729afbda05df384c04e3da629a0
ningweiwang <ningweiwang@sohu-inc.com>
2020-10-20 21:29:48 +0800

    update:jubao

--- src/views/ju-bao/children/20201026.vue
+++ src/views/ju-bao/children/20201026.vue
@@ -17,9 +17,39 @@ export default {
       day: '26',
       tableData: [
         {
+          area: '微信小程序3区',
+          uid: '187711',
+          img: 'http://www.anlan.xyz:8080/images/jubao/20201026/006.jpg',
+          times: '初犯',
+        },
+        {
           area: '微信小程序8区',
           uid: '68643110',
           img: 'http://www.anlan.xyz:8080/images/jubao/20201026/001.jpg',
+          times: '初犯',
+        },
+        {
+          area: '微信小程序11区',
+          uid: '31488175',
+          img: 'http://www.anlan.xyz:8080/images/jubao/20201026/002.jpg',
+          times: '第2次',
+        },
+        {
+          area: '空间2区',
+          uid: '5746177',
+          img: 'http://www.anlan.xyz:8080/images/jubao/20201026/004.jpg',
+          times: '第2次',
+        },
+        {
+          area: '空间4区',
+          uid: '5746177',
+          img: 'http://www.anlan.xyz:8080/images/jubao/20201026/003.jpg',
+          times: '第2次',
+        },
+        {
+          area: '空间4区',
+          uid: '2249892',
+          img: 'http://www.anlan.xyz:8080/images/jubao/20201026/005.jpg',
           times: '初犯',
         },
       ],
