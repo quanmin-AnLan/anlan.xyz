bf94da70dd356b7051c71b86446faef3c923e287
Poison-Frog <46836317+Poison-Frog@users.noreply.github.com>
2020-12-21 01:25:54 +0800

    update:jubao

--- src/views/ju-bao/children/20201221.vue
+++ src/views/ju-bao/children/20201221.vue
@@ -17,10 +17,52 @@ export default {
       day: '21',
       tableData: [
         {
-          area: '你猜',
-          uid: '你猜',
-          img: 'http://img.anlan.xyz/null.jpg?imagelim',
-          times: '你猜',
+          area: '微信小程序1区',
+          uid: '6974712',
+          img: 'http://img.anlan.xyz/ledou/jubao/2020/1221/8.jpg?imagelim',
+          times: '第3次',
+        },
+        {
+          area: '微信小程序11区',
+          uid: '28146164',
+          img: 'http://img.anlan.xyz/ledou/jubao/2020/1221/5.jpg?imagelim',
+          times: '第2次',
+        },
+        {
+          area: '微信小程序11区',
+          uid: '24755145',
+          img: 'http://img.anlan.xyz/ledou/jubao/2020/1221/7.jpg?imagelim',
+          times: '初犯',
+        },
+        {
+          area: '空间1区',
+          uid: '5335414',
+          img: 'http://img.anlan.xyz/ledou/jubao/2020/1221/2.jpg?imagelim',
+          times: '初犯',
+        },
+        {
+          area: '空间3区',
+          uid: '9245189',
+          img: 'http://img.anlan.xyz/ledou/jubao/2020/1221/3.jpg?imagelim',
+          times: '第2次',
+        },
+        {
+          area: '空间4区',
+          uid: '9519281',
+          img: 'http://img.anlan.xyz/ledou/jubao/2020/1221/1.jpg?imagelim',
+          times: '初犯',
+        },
+        {
+          area: '空间4区',
+          uid: '4842582',
+          img: 'http://img.anlan.xyz/ledou/jubao/2020/1221/4.jpg?imagelim',
+          times: '初犯',
+        },
+        {
+          area: '空间4区',
+          uid: '8485482',
+          img: 'http://img.anlan.xyz/ledou/jubao/2020/1221/6.jpg?imagelim',
+          times: '初犯',
         },
       ],
     };
