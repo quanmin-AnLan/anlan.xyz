b88156ca21366822d0002b1448418c4ee1ecd5ca
Poison-Frog <46836317+Poison-Frog@users.noreply.github.com>
2021-04-26 01:37:22 +0800

    update:lisa

--- src/views/lisa/lisa.vue
+++ src/views/lisa/lisa.vue
@@ -1,14 +1,17 @@
 <template>
   <section class="main">
     <diablo></diablo>
+    <Home></Home>
   </section>
 </template>
 
 <script>
 import Diablo from './components/diablo-compute.vue';
+import Home from '../../components/GoHome.vue';
 export default {
   components: {
     Diablo,
+    Home,
   },
   data() {
     return {
