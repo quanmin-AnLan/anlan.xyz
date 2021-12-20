e85819306ad36ad4684290fa6156431ab8a8658b
anlan <2016751527@qq.com>
2021-12-21 02:01:16 +0800

    fix:css

--- src/utils/jubao-tool.vue
+++ src/utils/jubao-tool.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="main">
     <header>{{ year }}年{{ month }}月{{ day }}日不文明行为备案</header>
-    <al-table :headerSet="headerSet" :tableData="tableData"></al-table>
+    <al-table :headerSet="headerSet" :tableData="listData"></al-table>
   </section>
 </template>
 
@@ -31,6 +31,7 @@ export default {
           label: '是否初犯',
         },
       ],
+      listData: [],
     };
   },
   props: {
@@ -50,6 +51,22 @@ export default {
       type: String,
       default: '',
     },
+  },
+  methods: {
+    washData(data) {
+      this.listData = data.map((item) => {
+        return {
+          area: item.area,
+          uid: item.uid,
+          img: item.img,
+          times: item.times,
+          colortimes: item.times === '初犯' ? '' : '#f00',
+        };
+      });
+    },
+  },
+  mounted() {
+    this.washData(this.tableData);
   },
 };
 </script>
